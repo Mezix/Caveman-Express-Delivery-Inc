91cd8ba49ce47b65e487e81b1221adb421f007bc
Felix Swimmer <mezteenx@googlemail.com>
2023-04-30 23:44:56 +0200

    added boxing glove anim and sound effect

--- Assets/Scripts/Player Scripts/BoxingArm.cs
+++ Assets/Scripts/Player Scripts/BoxingArm.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class BoxingArm : MonoBehaviour
 {
     public Transform _fist;
+    private Animator anim;
+    public AudioSource _punchSound;
 
     [HideInInspector] public float currentCooldown;
     [HideInInspector] public float punchCooldown;
@@ -19,13 +21,16 @@ public class BoxingArm : MonoBehaviour
         PunchStarted,
         OnCooldown
     }
-
+    private void Awake()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
     void Start()
     {
         _punchState = PunchState.ReadyToPunch;
         ShowFist(false);
         currentCooldown = 0;
-        punchCooldown = 50f;
+        punchCooldown = 25f;
     }
     void Update()
     {
@@ -63,13 +68,16 @@ public class BoxingArm : MonoBehaviour
     public void ShowFist(bool show)
     {
         _fist.gameObject.SetActive(show);
-
     }
     public void StartPunch()
     {
         currentCooldown = punchCooldown;
         _punchState = PunchState.OnCooldown;
         ShowFist(true);
+        anim.SetTrigger("Punch");
+
+        _punchSound.pitch = UnityEngine.Random.Range(0.8f, 1.2f);
+        _punchSound.Play();
 
         Vector3 punchDir = Input.mousePosition;
         punchDir = Camera.main.ScreenToWorldPoint(punchDir);
