661f607ca4668e3a2b0d3c5a9c19afdd0751951b
notBigL <99298254+notBigL@users.noreply.github.com>
2023-05-02 01:21:37 +0200

    Lots of convo stuff

--- Assets/Scripts/LevelInfo.cs
+++ Assets/Scripts/LevelInfo.cs
@@ -7,14 +7,24 @@ public class LevelInfo : MonoBehaviour
     public float _timeInSecondsForThisLevel;
     public bool shouldStartTimer;
     public Loader.Scene _thisScene;
+    public ConversationScriptObj convoOnLoad;
 
     public bool shouldResetScoreAndTime;
     private void Start()
     {
+        StartCoroutine(playDialogue());
         Loader._currentScene = _thisScene;
-
         if (!REF.score) return;
         REF.score.SetCurrentTime(this);
         REF.score.StartTimer(shouldStartTimer);
+    }
+
+    private IEnumerator playDialogue()
+    {
+        for (int i = 0; i <= 50; i++)
+        {
+            yield return new WaitForFixedUpdate();
+        }
+        if (REF.dialog) REF.dialog.StartDialogue(convoOnLoad);
     }
 }
