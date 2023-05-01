37ce5bdb89a402c02dc8d6c92059ab0738ac5ef7
Felix Swimmer <mezteenx@googlemail.com>
2023-05-02 01:39:18 +0200

    updated dialogue UI

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -6,7 +6,6 @@ using UnityEngine.UI;
 public class ButtonScript : MonoBehaviour
 {
     public Button _button;
-    public Text _text;
     public Image _graphic;
 
     public AudioSource _hoverSound;
--- Assets/Scripts/SliderScript.cs
+++ Assets/Scripts/SliderScript.cs
@@ -5,5 +5,17 @@ using UnityEngine.UI;
 
 public class SliderScript : MonoBehaviour
 {
+    public AudioSource _hoverSound;
+    public AudioSource _clickSound;
+
     public Slider _slider;
+
+    public void Hover()
+    {
+        _hoverSound.Play();
+    }
+    public void Click()
+    {
+        _clickSound.Play();
+    }
 }
