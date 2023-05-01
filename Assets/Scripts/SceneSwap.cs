82c2ebdb163e54384ff0c4f42db00a074eebd36b
Felix Swimmer <mezteenx@googlemail.com>
2023-05-01 14:18:28 +0200

    fixed player collider, added scene swap

---
+++ Assets/Scripts/SceneSwap.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSwap : MonoBehaviour
+{
+    public Loader.Scene nextScene;
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.TryGetComponent(out PlayerController p))
+        {
+            Debug.Log("Loading : " + nextScene);
+            Loader.Load(nextScene);
+        }
+    }
+}
