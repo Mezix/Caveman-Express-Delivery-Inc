4033b82f0413cb78f797528cf1c0b44190641723
Felix Swimmer <mezteenx@googlemail.com>
2023-05-01 20:57:52 +0200

    changes to package dispenser

--- Assets/Scripts/GameSceneScripts/PackageDispenser.cs
+++ Assets/Scripts/GameSceneScripts/PackageDispenser.cs
@@ -9,6 +9,7 @@ public class PackageDispenser : MonoBehaviour
     public SpriteRenderer sr;
     public Transform _dispenseTransform;
     public float _dispenseForce;
+    public float _dispenseStartDistance;
 
     public float timeSinceLastDispensation;
     public float dispenserCooldown;
@@ -18,6 +19,7 @@ public class PackageDispenser : MonoBehaviour
 
     public List<PhysicsPackage> _packagesCreated = new List<PhysicsPackage>();
     public DispenserDirection dispenserDir;
+    public AudioSource _dispenseSound;
     public enum DispenserDirection
     {
         Left,
@@ -29,6 +31,7 @@ public class PackageDispenser : MonoBehaviour
     {
         _dispenseForce = 5;
         dispenserCooldown = 2;
+        _dispenseStartDistance = 1f;
     }
     private void Start()
     {
@@ -40,22 +43,22 @@ public class PackageDispenser : MonoBehaviour
         switch (dispenserDir)
         {
             case DispenserDirection.Left:
-                _dispenseTransform.localPosition = new Vector2(-0.5f, 0);
+                _dispenseTransform.localPosition = new Vector2(-_dispenseStartDistance, 0);
                 HM.RotateLocalTransformToAngle(_dispenseTransform, new Vector3(0,0,180));
                 sr.sprite = HM.GetSpriteFromSpritesheet(GS.Props("Dispenser_SpriteSheet"), "Dispenser_SpriteSheet_2");
                 break;
             case DispenserDirection.Up:
-                _dispenseTransform.localPosition = new Vector2(0, 0.5f);
+                _dispenseTransform.localPosition = new Vector2(0, _dispenseStartDistance);
                 HM.RotateLocalTransformToAngle(_dispenseTransform, new Vector3(0, 0, 90));
                 sr.sprite = HM.GetSpriteFromSpritesheet(GS.Props("Dispenser_SpriteSheet"), "Dispenser_SpriteSheet_3");
                 break;
             case DispenserDirection.Down:
-                _dispenseTransform.localPosition = new Vector2(0, -0.5f);
+                _dispenseTransform.localPosition = new Vector2(0, _dispenseStartDistance);
                 HM.RotateLocalTransformToAngle(_dispenseTransform, new Vector3(0, 0, 270));
                 sr.sprite = HM.GetSpriteFromSpritesheet(GS.Props("Dispenser_SpriteSheet"), "Dispenser_SpriteSheet_0");
                 break;
             default: //default = Right 
-                _dispenseTransform.localPosition = new Vector2(0.5f, 0);
+                _dispenseTransform.localPosition = new Vector2(_dispenseStartDistance, 0);
                 HM.RotateLocalTransformToAngle(_dispenseTransform, new Vector3(0, 0, 0));
                 sr.sprite = HM.GetSpriteFromSpritesheet(GS.Props("Dispenser_SpriteSheet"), "Dispenser_SpriteSheet_1");
                 break;
@@ -78,6 +81,8 @@ public class PackageDispenser : MonoBehaviour
 
     public void DispensePackage()
     {
+        _dispenseSound.pitch = HM.GetFloatWithRandomVariance(_dispenseSound.pitch, 0.2f);
+        _dispenseSound.Play();
         timeSinceLastDispensation = 0;
         GameObject g = ProjectilePool.Instance.GetProjectileFromPool(_packagePrefab.tag);
         g.transform.position = _dispenseTransform.position;
